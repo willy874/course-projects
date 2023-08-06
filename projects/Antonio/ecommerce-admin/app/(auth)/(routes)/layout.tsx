c0752cd2c6644c0b330b7c0a2314bf7003267118
hcw0915 <love233031@gmail.com>
2023-08-06 16:14:09 +0800

    delete the Next project

--- projects/Antonio/ecommerce-admin/app/(auth)/(routes)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <div className="flex items-center justify-center h-full">{children}</div>
-  )
-}
